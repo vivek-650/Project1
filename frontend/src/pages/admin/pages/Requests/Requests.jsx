4b50e7e79c4024199158ed4855e754d690525c7e
Satendra kumar <satyendrakumar.contact@gmail.com>
2025-02-13 01:49:57 +0530

    fix: layout fix

--- frontend/src/pages/admin/pages/Requests/Requests.jsx
+++ frontend/src/pages/admin/pages/Requests/Requests.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Requests = () => {
-  return <div>Requests</div>;
+  return (
+    <div>
+      <h1>Requests</h1>
+    </div>
+  );
 };
--- frontend/src/pages/admin/pages/Users/Users.jsx
+++ frontend/src/pages/admin/pages/Users/Users.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Users = () => {
-  return <div>Users</div>;
+  return (
+    <div>
+      <h1>Users</h1>
+    </div>
+  );
 };
