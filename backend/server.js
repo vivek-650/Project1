33f4b8b0d91045a265d308cf16cdfcaf7e39e74c
Satendra kumar <satyendrakumar.contact@gmail.com>
2025-02-13 02:35:27 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors()); // Allow requests from your frontend
 app.use(express.json()); // Parse JSON bodies
 
+app.get("/", (req, res) => {
+  res.send("Server is Running");
+});
 
 const PORT = process.env.PORT || 4040;
 app.listen(PORT, () => {
