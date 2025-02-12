00822f06457c735018a7c15de2a031f0f0651dcb
Vivek <vivek66anand66@gmail.com>
2025-02-13 01:56:54 +0530

    user dashboard route fix

--- frontend/src/pages/routes/UserRoutes.jsx
+++ frontend/src/pages/routes/UserRoutes.jsx
@@ -1,16 +1,21 @@
 // import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "../user/components/Layout";
-// import Dashboard from "../user/pages/Dashboard/Dashboard";
 import Dashboard from "../user/pages/dashboard/Dashboard";
+import Recepies from "../user/pages/Recepies/Recepies";
+import Drafts from "../user/pages/drafts/drafts";
+import Uploaded from "../user/pages/uploaded/Uploaded";
+import AddRecepies from "../user/pages/AddRecepies/AddRecepies";
 
 const UserRoutes = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route index path={"/dashboard"} element={<Dashboard />} />
-        {/* <Route path={"/users"} element={<Users />} /> */}
-        {/* <Route path={"/requests"} element={<Requests />} /> */}
+        <Route path={"/recepies"} element={<Recepies />} />
+        <Route path={"/drafts"} element={<Drafts />} />
+        <Route path={"/uploaded"} element={<Uploaded />} />
+        <Route path={"/new-recepie"} element={<AddRecepies />} />
       </Route>
     </Routes>
   )
---
+++ frontend/src/pages/user/pages/AddRecepies/AddRecepies.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+const AddRecepies = () => {
+  return (
+    <div>AddRecepies</div>
+  )
+}
+
+export default AddRecepies
